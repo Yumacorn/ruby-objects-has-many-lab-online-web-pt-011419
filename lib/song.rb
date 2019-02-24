13872f8559cd263d0a0284889ebfe3eed1f4eb9b
Ryan Mui <reachryanmui@gmail.com>
2019-02-24 16:36:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
   attr_accessor :name, :artist
-  @@song_count = 0
+  # @@song_count = 0
 
   def initialize(name)
     @name = name
-    @@song_count += 1
+    # @@song_count += 1
   end
 
   # def add_song(name)
